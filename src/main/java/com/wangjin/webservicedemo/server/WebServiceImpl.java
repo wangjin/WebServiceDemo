f7f3e0154b25ff3770975e3d32605b376a1bb601
WangJin <wangjin-252@hotmail,com>
2014-09-29 23:22:53 +0800

    Spring @Component 注解

--- src/main/java/com/wangjin/webservicedemo/server/WebServiceImpl.java
+++ src/main/java/com/wangjin/webservicedemo/server/WebServiceImpl.java
@@ -1,8 +1,14 @@
 package com.wangjin.webservicedemo.server;
 
+import org.springframework.stereotype.Component;
+
+import javax.jws.WebService;
+
 /**
  * Created by wangjin on 14-9-29.
  */
+@Component("DemoWebserviceImpl")
+@WebService(name="DEMO_WEBSERVICE",targetNamespace = "http://demo.webservice.com")
 public class WebServiceImpl implements WebServiceInterface {
 
     @Override
